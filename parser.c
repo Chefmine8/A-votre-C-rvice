fd6c52d3e323bf19f1655bd7191e61f1134ef06d
Chefmine <ChMi8@pm.me>
2025-09-15 17:33:59 +0200

    upper and start of file reader

--- parser.c
+++ parser.c
@@ -9,8 +9,32 @@ void upper(char *grid) {
     }
 }
 
-void main() {
-    char *grid = "awerz";
+char **read_file(char *file) {
+    FILE *fichier = fopen(file, "r");
+    size_t n_ligne = 1;
+    size_t n_c = 1;
+    size_t i_ligne = 0;
+    size_t i_c = 0;
+    char **matrice = malloc(n_ligne);
+    int c;
+
+    while ((c = fgetc(fichier)) != EOF) {
+        if (c == '\n') {
+            if (++i_ligne > n_ligne) matrice = realloc(matrice, ++n_ligne * sizeof(char*));
+            matrice++;
+            n_c = 1;
+            *matrice = malloc(n_c);
+            continue;
+        }
+        if (++i_c > n_c) *matrice = realloc(*matrice, ++n_c * sizeof(char));
+    }
+}
+
+
+
+int main() {
+    char grid[] = "awerz";
     upper(grid);
     printf("%s", grid);
+    return 0;
 }
