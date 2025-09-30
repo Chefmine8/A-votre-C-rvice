bc2998c64537b245d39b6f50653fe3c9690a8afa
Pierre Mittelbronn <chefmine@pm.me>
2025-09-30 18:33:09 +0200

    final

--- parser.h
+++ parser.h
@@ -1,5 +1,8 @@
 #ifndef PARSER_H
 #define PARSER_H
     void upper(char *grid);
-
+    char **read_file(char *file);
+    void free_matrix(char **matrice);
+    void solver_algorithme(char **grid, char *word);
+    int *solv_rec(char **grid, char *word, int inc_x, int inc_y, int x, int y, int h, int w);
 #endif
